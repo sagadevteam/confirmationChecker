73d7bf653bfedcff4575578955057d538d4a1fe7
Brian <brian770130@gmail.com>
2018-08-19 12:59:43 -0700

    Add mysql package and processed block operating function

--- checker.js
+++ checker.js
@@ -1,13 +1,94 @@
 const Web3 = require('web3');
+const mysql = require('mysql');
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-const address = "0xb0aa80e9f5f8052ab2af46071ad3258e3fe807e9";
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "saga"
+});
 
-let balance = web3.eth.getBalance(address);
+var createProcessedBlock = (blk) => {
+  return new Promise((resolve, reject) => {
+    con.connect((err) => {
+      if (err) {
+        reject(err);
+      } else {
+        var sql = `INSERT INTO processed_block (block_height) VALUES (${blk})`;
+        con.query(sql, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        });
+      }
+    });
+  });
+}
 
-console.log(balance)
+var updateProcessedBlock = (blk) => {
+  return new Promise((resolve, reject) => {
+    con.connect((err) => {
+      if (err) {
+        reject(err);
+      } else {
+        var sql = `UPDATE processed_block SET block_height = ${blk} WHERE id = 1`;
+        con.query(sql, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        });
+      }
+    });
+  });
+}
 
+var getProcessedBlock = () => {
+  return new Promise((resolve, reject) => {
+    con.connect((err) => {
+      if (err) {
+        reject(err);
+        // error handle
+      } else {
+        con.query("SELECT * FROM processed_block WHERE id = 1", (err, result) => {
+          if (err) {
+            reject(err);
+            // error handle
+          } else {
+            var value = ((result.length == 0) ? 0 : result[0].block_height);
+            resolve(value);
+          }
+        });
+      }
+    });
+  });
+}
+
+var main = async () => {
+  try {
+    var processedBlock = await getProcessedBlock();
+    if (processedBlock == 0) {
+      let blk = web3.eth.getBlock("latest")
+      var create = false;
+      while (!create) {
+        create = createProcessedBlock(blk.number);
+      }
+    }
+    console.log("ProcessedBlock: ", processedBlock);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+//let blk = web3.eth.getBlock("latest")
+
+//console.log(blk.number)
+/*
 const filter = web3.eth.filter('latest');
 filter.watch((err, res) => {
   if (err) {
@@ -24,3 +105,6 @@ filter.watch((err, res) => {
     });
   }
 });
+*/
+
+main()
