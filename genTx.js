98a29fd07e0a11c3e5cdbd65c762689fbfc1c3e9
Brian <brian770130@gmail.com>
2018-08-23 11:02:53 -0700

    modify genTx.js simpler

--- genTx.js
+++ genTx.js
@@ -7,13 +7,11 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 } 
 
-for (let i = 0; i < 1000; i++) {
-	var _from = users[getRandomInt(users.length)];
+for (let i = 0; i < 50; i++) {
+	var _from = users[0];
 	var _to = users[getRandomInt(users.length)];
-	var _max = web3.eth.getBalance(_from);
-	_max = (_max >= 100) ? 100  : _max;
-	var _value = getRandomInt(_max);
+	var _value = getRandomInt(100);
 	web3.personal.unlockAccount(_from, "");
-	console.log(_from,"==>", _to,", value:", _value);
+	console.log("address: ", _to,", value:", _value);
 	web3.eth.sendTransaction({"from": _from, "to": _to, "value": _value, "gasPrice": 0});
 }
